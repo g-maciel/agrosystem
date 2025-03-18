a4bb03b8e803f1fc7bb709f331f183fd127f67db
g-maciel <g.maciel@icb-systems.com>
2025-03-19 00:28:24 +0100

    Scripts

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Waiting for database connection..."
+until nc -z -v -w30 $DB_HOST $DB_PORT; do
+    echo "Waiting for database connection..."
+    sleep 2
+done
+
+echo "Database is ready!"
+
+php artisan migrate --force
+
+php artisan serve --host=0.0.0.0 --port=8000
